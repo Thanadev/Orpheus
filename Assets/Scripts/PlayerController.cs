40d46e1def96e58508faa26c6c8421c652aabe31
Thanadev <thomas.lambert33720@gmail.com>
2017-06-21 15:08:50 +0200

    Init game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Thanagames.Damocles.Controllers {
+
+	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent(typeof(Collider))]
+	public class PlayerController : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Thanagames.Damocles.Controllers {
+	
+	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent(typeof(Collider))]
+	public class SwordController : MonoBehaviour {
+
+		public float fallingSpeed;
+
+		protected Rigidbody rb;
+
+		// Use this for initialization
+		void Awake () {
+			rb = GetComponent<Rigidbody> ();
+		}
+
+		void Start () {
+			rb.velocity = new Vector3 (0, -fallingSpeed, 0);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
