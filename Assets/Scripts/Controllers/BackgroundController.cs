88f7645ebdf1dada133504f54211000ea1a87e66
Thanadev <thomas.lambert33720@gmail.com>
2017-06-26 18:20:45 +0200

    Ajusted graphics and difficulty

---
+++ Assets/Scripts/Controllers/BackgroundController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+	public float scrollSpeed = 0.1f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y - scrollSpeed, transform.position.z);
+	}
+}
